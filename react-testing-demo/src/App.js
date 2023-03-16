77d981ad8222304c3710cf0dc1ca2f3a5ce5e2b2
Pankaj Sharma <36689931+code-gram@users.noreply.github.com>
2023-03-16 08:47:56 +0530

    Initial Code for Testing React Use Cases

---
+++ react-testing-demo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Testing Updated</h1>
+      <h2>New Changes!!</h2>
+    </div>
+  );
+}
+
+
+export default App;
